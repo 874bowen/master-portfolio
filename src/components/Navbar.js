7444e330e3ff0c9a9b38536e172ccf3e4d12585e
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-19 16:23:07 +0300

    Navbar initial setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,47 @@
-// src/components/Navbar.js
+import React, { Component } from 'react';
+import { Container, Navbar, NavDropdown, FormControl, Nav,
+    Button,
+    Form} from 'react-bootstrap';
+// import { NavLink } from 'react-router-dom';
 
-export default function Navbar() {}
+export default function Navba() {
+    return(
+ 
+    <Navbar sticky="top" id="navba" expand="lg">
+        <Container>
+            <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+            <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+            >
+                <Nav.Link href="#">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+                <NavDropdown title="Link" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                    Something else here
+                </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#" disabled>
+                Link
+                </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+                <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+            </Form>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+    );
+}
