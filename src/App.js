9b6983cf0653cb0063fe19bc468beb6905ea0a93
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-21 17:31:39 +0300

    App.js as parent class and passing the state to child class ProjectComponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
@@ -8,17 +8,28 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Project from './components/ProjectComponent';
 import Testimonials from "./components/Testimonials";
+import { PROJECTS } from "./shared/projects";
 
-function App() {
-  return (
-    <main>
-      <Navbar />
-      <About />
-      <Project />
-      <Testimonials />
-      <Contact />
-    </main>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      projects: PROJECTS
+    };
+  }
+  render(){
+    return (
+      <main>
+        <Navbar />
+        <About />
+        <Project projects= {this.state.projects}/>
+        <Testimonials />
+        <Contact />
+      </main>
+    );
+  }
+  
 }
 
 export default App;
