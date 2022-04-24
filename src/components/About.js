cd18c3a9bb9ace6792c421a6c080045e915438e6
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-24 09:58:46 +0300

    added some nice styling to the about component

--- src/components/About.js
+++ src/components/About.js
@@ -4,14 +4,14 @@ import { Button } from "react-bootstrap"
 export default function About() {
     return (
         
-        <section id="about" mb-10>
+        <section id="about">
           <div className="container">
             <div className="row">
               <div className="align-self-center col-12 col-md-6">
               <h1 className="text-white">
                 Hi, I'm <span className="text-success">Ivan Bowen</span>.
               </h1>
-              <h2>I love <span class="brick">building</span> amazing
+              <h2>I love <span className="brick">building</span> amazing
                 web apps.</h2>
               <p className="">
               Enthusiastic Web developer eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of web development and designing and training in Responsive Web Design. Motivated to learn, grow and excel in Technology.
@@ -41,9 +41,9 @@ export default function About() {
             </div>
 
           </div>
-          <section class="mt-40 col-12 custom-shape-divider-bottom-1650314778">
+          <section className="mt-40 col-12 custom-shape-divider-bottom-1650314778">
             <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
             </svg>
         </section>
         </section>
