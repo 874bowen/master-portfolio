f8a5b470f455ef21795784b191ecb39369759d0d
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-25 09:38:15 +0300

    migrated the comments to comments.js

--- src/shared/projects.js
+++ src/shared/projects.js
@@ -8,47 +8,7 @@ export const PROJECTS = [
       link: "https://reactbootcamp.com",
       gif: './images/bootstrap-con.gif',
       description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',     
-      comment: [
-        {
-            id: 0,
-            dishId: 0,
-            rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
-            author: "John Lemon",
-            date: "2012-10-16T17:57:28.556094Z"
-        },
-        {
-            id: 1,
-            dishId: 0,
-            rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Paul McVites",
-            date: "2014-09-05T17:57:28.556094Z"
-        },
-        {
-            id: 2,
-            dishId: 0,
-            rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Michael Jaikishan",
-            date: "2015-02-13T17:57:28.556094Z"
-        },
-        {
-            id: 3,
-            dishId: 0,
-            rating: 4,
-            comment: "Ultimate, Reaching for the stars!",
-            author: "Ringo Starry",
-            date: "2013-12-02T17:57:28.556094Z"
-        },
-        {
-            id: 4,
-            dishId: 0,
-            rating: 2,
-            comment: "It's your birthday, we're gonna party!",
-            author: "25 Cent",
-            date: "2011-12-02T17:57:28.556094Z"
-        }]                   },
+      },
    {
       id: 1,
       title:'Code Snippets App',
@@ -58,48 +18,7 @@ export const PROJECTS = [
       link: "https://reactbootcamp.com",
       gif: './images/bootstrap-con.gif',
       description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-      comment: [
-      {
-        id: 5,
-        dishId: 1,
-        rating: 5,
-        comment: "Imagine all the eatables, living in conFusion!",
-        author: "John Lemon",
-        date: "2012-10-16T17:57:28.556094Z"
-    },
-    {
-        id: 6,
-        dishId: 1,
-        rating: 4,
-        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-        author: "Paul McVites",
-        date: "2014-09-05T17:57:28.556094Z"
-    },
-    {
-        id: 7,
-        dishId: 1,
-        rating: 3,
-        comment: "Eat it, just eat it!",
-        author: "Michael Jaikishan",
-        date: "2015-02-13T17:57:28.556094Z"
-    },
-    {
-        id: 8,
-        dishId: 1,
-        rating: 4,
-        comment: "Ultimate, Reaching for the stars!",
-        author: "Ringo Starry",
-        date: "2013-12-02T17:57:28.556094Z"
-    },
-    {
-        id: 9,
-        dishId: 1,
-        rating: 2,
-        comment: "It's your birthday, we're gonna party!",
-        author: "25 Cent",
-        date: "2011-12-02T17:57:28.556094Z"
-    }
-    ]},
+      },
    {
       id: 2,
       title:'Ads Management',
@@ -109,48 +28,7 @@ export const PROJECTS = [
       link: "https://reactbootcamp.com",
       gif: './images/bootstrap-con.gif',
       description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-      comment: [
-        {
-            id: 10,
-            dishId: 2,
-            rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
-            author: "John Lemon",
-            date: "2012-10-16T17:57:28.556094Z"
-        },
-        {
-            id: 11,
-            dishId: 2,
-            rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Paul McVites",
-            date: "2014-09-05T17:57:28.556094Z"
-        },
-        {
-            id: 12,
-            dishId: 2,
-            rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Michael Jaikishan",
-            date: "2015-02-13T17:57:28.556094Z"
-        },
-        {
-            id: 13,
-            dishId: 2,
-            rating: 4,
-            comment: "Ultimate, Reaching for the stars!",
-            author: "Ringo Starry",
-            date: "2013-12-02T17:57:28.556094Z"
-        },
-        {
-            id: 14,
-            dishId: 2,
-            rating: 2,
-            comment: "It's your birthday, we're gonna party!",
-            author: "25 Cent",
-            date: "2011-12-02T17:57:28.556094Z"
-        }
-      ]                         },
+      },
    {
       id: 3,
       title:'Con Fusion Restaurant',
@@ -159,49 +37,7 @@ export const PROJECTS = [
       category: 'backend',
       link: "https://reactbootcamp.com",
       gif: './images/bootstrap-con.gif',
-      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-      comment: [
-        {
-            id: 15,
-            dishId: 3,
-            rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
-            author: "John Lemon",
-            date: "2012-10-16T17:57:28.556094Z"
-        },
-        {
-            id: 16,
-            dishId: 3,
-            rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Paul McVites",
-            date: "2014-09-05T17:57:28.556094Z"
-        },
-        {
-            id: 17,
-            dishId: 3,
-            rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Michael Jaikishan",
-            date: "2015-02-13T17:57:28.556094Z"
-        },
-        {
-            id: 18,
-            dishId: 3,
-            rating: 4,
-            comment: "Ultimate, Reaching for the stars!",
-            author: "Ringo Starry",
-            date: "2013-12-02T17:57:28.556094Z"
-        },
-        {
-            id: 19,
-            dishId: 3,
-            rating: 2,
-            comment: "It's your birthday, we're gonna party!",
-            author: "25 Cent",
-            date: "2011-12-02T17:57:28.556094Z"
-        }
-    ]                       
+      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',                     
     },
     {
         id: 4,
@@ -211,48 +47,6 @@ export const PROJECTS = [
         category: 'backend',
         link: "https://reactbootcamp.com",
         gif: './images/bootstrap-con.gif',
-        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-        comment: [
-          {
-              id: 20,
-              dishId: 3,
-              rating: 5,
-              comment: "Imagine all the eatables, living in conFusion!",
-              author: "John Lemon",
-              date: "2012-10-16T17:57:28.556094Z"
-          },
-          {
-              id: 21,
-              dishId: 3,
-              rating: 4,
-              comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-              author: "Paul McVites",
-              date: "2014-09-05T17:57:28.556094Z"
-          },
-          {
-              id: 22,
-              dishId: 3,
-              rating: 3,
-              comment: "Eat it, just eat it!",
-              author: "Michael Jaikishan",
-              date: "2015-02-13T17:57:28.556094Z"
-          },
-          {
-              id: 23,
-              dishId: 3,
-              rating: 4,
-              comment: "Ultimate, Reaching for the stars!",
-              author: "Ringo Starry",
-              date: "2013-12-02T17:57:28.556094Z"
-          },
-          {
-              id: 24,
-              dishId: 3,
-              rating: 2,
-              comment: "It's your birthday, we're gonna party!",
-              author: "25 Cent",
-              date: "2011-12-02T17:57:28.556094Z"
-          }
-      ]                       
+        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',                     
       }
 ]
