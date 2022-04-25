4ced36357efc8914a6538d64ba7dd8cd0e9a9947
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-25 09:43:00 +0300

    shifted comments to a new file and added a blogs.js to carry blogs information

---
+++ src/shared/comments.js
@@ -0,0 +1,243 @@
+export const COMMENTS =
+[
+    {
+        id: 0,
+        projectId: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 1,
+        projectId: 0,
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 2,
+        projectId: 0,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 3,
+        projectId: 0,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 4,
+        projectId: 0,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 5,
+        projectId: 1,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 6,
+        projectId: 1,
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 7,
+        projectId: 1,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 8,
+        projectId: 1,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 9,
+        projectId: 1,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 10,
+        projectId: 2,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 11,
+        projectId: 2,
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 12,
+        projectId: 2,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 13,
+        projectId: 2,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 14,
+        projectId: 2,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 15,
+        projectId: 3,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 16,
+        projectId: 3,
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 17,
+        projectId: 3,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 18,
+        projectId: 3,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 19,
+        projectId: 3,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 20,
+        projectId: 4,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 21,
+        projectId: 4,
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 22,
+        projectId: 4,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 23,
+        projectId: 4,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 24,
+        projectId: 4,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 25,
+        projectId: 5,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "John Lemon",
+        date: "2012-10-16T17:57:28.556094Z"
+    },
+    {
+        id: 16,
+        projectId: 3,
+        rating: 4,
+        comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Paul McVites",
+        date: "2014-09-05T17:57:28.556094Z"
+    },
+    {
+        id: 26,
+        projectId: 5,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Michael Jaikishan",
+        date: "2015-02-13T17:57:28.556094Z"
+    },
+    {
+        id: 27,
+        projectId: 5,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Ringo Starry",
+        date: "2013-12-02T17:57:28.556094Z"
+    },
+    {
+        id: 28,
+        projectId: 5,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "25 Cent",
+        date: "2011-12-02T17:57:28.556094Z"
+    }
+];
