fea02430602f071b846eb98b58ee7d241207979b
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-25 09:36:03 +0300

    introducing react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import React, { Component } from 'react';
 import Main from "./components/MainComponent";
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   
   render(){
     return (
-      <main>
-        <Main />
-      </main>
+      <BrowserRouter>
+        <main>
+          <Main />
+        </main>
+       </BrowserRouter>
     );
   }
   
