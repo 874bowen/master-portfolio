aa04fd97679aa3db88a31e094924f7e0b2165ad5
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-27 21:52:22 +0300

    added blogs.js

---
+++ src/shared/blogs.js
@@ -0,0 +1,22 @@
+export const BLOGS = [
+    {
+        id: 0,
+        title:'Docstrings in Python for begginners',
+        subtitle: 'Python',
+        image: './images/dj-ads.jpg',
+        link: "https://reactbootcamp.com",
+        date: "2022-04-23T19:50:28.346094Z",
+        description:'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...',
+        featured: true,     
+    },
+    {
+        id: 1,
+        title:'Object Oriented Programming in Python',
+        subtitle: 'Python',
+        image: './images/dj-ads.jpg',
+        link: "https://reactbootcamp.com",
+        date: "2022-04-30T19:50:28.346094Z",
+        description:'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...',
+        featured: false,     
+    }
+]
