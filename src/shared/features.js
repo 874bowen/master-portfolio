466618e7167b3334324072cb03117ebc6d579fa5
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-27 21:53:25 +0300

    added features.js

---
+++ src/shared/features.js
@@ -0,0 +1,26 @@
+export const FEATURES = [
+    {
+        id: 0,
+        sector:'Front-end development',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: './images/bowen.jpeg',
+        motivation: "#creativity",
+        framework: 'react.js'     
+    },
+    {
+        id: 1,
+        sector:'API',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: './images/bowen.jpeg',
+        motivation: "#communication",
+        framework: 'FastAPI'     
+    },
+    {
+        id: 2,
+        sector:'Back-end',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        image: './images/bowen.jpeg',
+        motivation: "#authentic",
+        framework: 'Django'     
+    }
+]
