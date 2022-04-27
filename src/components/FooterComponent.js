1d8b511fca2e00b879d8ba1f0178a6c0a345a6a6
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-27 05:09:37 +0300

    added instagram and GitHub icon

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -43,11 +43,11 @@ export default function Footer() {
           </div>
           <div className="col-12 col-sm-4 col-md-3 align-self-center">
           <div className="text-center">
-                        <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-google-plus"></i></a>
+                        <a className="btn btn-social-icon btn-google" href="http://google.com/+"><i className="fa fa-instagram"></i></a>
                         <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><i className="fa fa-facebook"></i></a>
                         <a className="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><i className="fa fa-linkedin"></i></a>
                         <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter"></i></a>
-                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube"></i></a>
+                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-github"></i></a>
                         <a className="btn btn-social-icon" href="mailto:"><i className="fa fa-envelope-o"></i></a>
                     </div>
             </div>
