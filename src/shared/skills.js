c05caed185a0bfdaa40ab1c9dc14529f9e87e46e
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-27 21:54:04 +0300

    added skills.js

---
+++ src/shared/skills.js
@@ -0,0 +1,53 @@
+export const SKILLS = 
+[
+    {
+        id: 0,
+        width: "80%",
+        placeholder: "HTML 80%"
+    },
+    {
+        id: 1,
+        width: "50%",
+        placeholder: "CSS 50%"
+    },
+    {
+        id: 2,
+        width: "60%",
+        placeholder: "JS 60%"
+    },
+    {
+        id: 3,
+        width: "70%",
+        placeholder: "SQL 70%"
+    },
+    {
+        id: 4,
+        width: "67%",
+        placeholder: "REACT 67%"
+    },
+    {
+        id: 5,
+        width: "70%",
+        placeholder: "PHP 70%"
+    },
+    {
+        id: 6,
+        width: "60%",
+        placeholder: "POSTGRESQL 60%"
+    },
+    {
+        id: 7,
+        width: "75%",
+        placeholder: "PYTHON 75%"
+    },
+    {
+        id: 8,
+        width: "76%",
+        placeholder: "DJANGO 76%"
+    },
+    {
+        id: 9,
+        width: "85%",
+        placeholder: "BOOTSTRAP 85%"
+    },
+]
