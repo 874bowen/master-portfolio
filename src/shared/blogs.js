b2bd2f3e8916b577b43781a2ba4a1c236292c27a
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-04-29 09:34:21 +0300

    Some changes on image to be rendered on the projects.js and blogs.js

--- src/shared/blogs.js
+++ src/shared/blogs.js
@@ -3,10 +3,10 @@ export const BLOGS = [
         id: 0,
         title:'Docstrings in Python for begginners',
         subtitle: 'Python',
-        image: './images/dj-ads.jpg',
+        image: './images/docpy.jpeg',
         link: "https://medium.com/@ivan_bowen/docstrings-in-python-for-beginners-9bdd2657f8c4",
         date: "2022-04-23T19:50:28.346094Z",
-        description:'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...',
+        description:'So, what exactly is documentation? What are docstrings? Why is it recommended more frequently during Python programming documentation? What is the best method for documenting your ...',
         featured: true,     
     },
     {
