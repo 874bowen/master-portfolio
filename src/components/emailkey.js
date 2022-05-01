eb7f87752699304fa7510b09e0f751d41352cb3b
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-01 21:15:59 +0300

    trial purposes emailjs

---
+++ src/components/emailkey.js
@@ -0,0 +1,6 @@
+// environment variables trial
+
+export default {
+    USER_ID: '{process.env.REACT_APP_MY_USER_ID}', //userID
+    TEMPLATE_ID: '{process.env.REACT_APP_TEMPLATE_ID}'         //templateID
+}
