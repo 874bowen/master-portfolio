8cc9372b6fe6e660cf0aa1de6807c6667daacf21
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-07 21:51:02 +0300

    react redux configuration provider with store

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import './App.css';
 import React, { Component } from 'react';
 import Main from "./components/MainComponent";
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigStore } from './redux/configStore';
+
+const store = ConfigStore();
 
 class App extends Component {
   
   render(){
     return (
+      <Provider store={store}>
       <BrowserRouter>
         <main>
           <Main />
         </main>
        </BrowserRouter>
+       </Provider>
     );
   }
   
