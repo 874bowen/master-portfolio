3ec7de7c9e9e2d5547d93c4e2ad9fc010049b546
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-05-08 17:13:24 +0300

    for oop blog

--- src/shared/blogs.js
+++ src/shared/blogs.js
@@ -1,22 +1,22 @@
 export const BLOGS = [
     {
-        id: 0,
+        id: 1,
+        title:'Object Oriented Programming in Python',
+        subtitle: 'Python',
+        image: './images/dj-ads.jpg',
+        link: "https://medium.com/@ivan_bowen/object-oriented-concepts-in-python-ec6f3652a42b",
+        date: "2022-05-08T16:50:28.346094Z",
+        description:'Object Oriented approach answers the question who are the key players(objects)- Shapes in our example above; circle, square and triangle. Examples of object oriented programming...',
+        featured: false,     
+    },
+    {
+        id: 2,
         title:'Docstrings in Python for begginners',
         subtitle: 'Python',
-        image: './images/docpy.jpeg',
+        image: './images/programming_approaches.jpg',
         link: "https://medium.com/@ivan_bowen/docstrings-in-python-for-beginners-9bdd2657f8c4",
         date: "2022-04-23T19:50:28.346094Z",
         description:'So, what exactly is documentation? What are docstrings? Why is it recommended more frequently during Python programming documentation? What is the best method for documenting your ...',
         featured: true,     
     },
-    {
-        id: 1,
-        title:'Object Oriented Programming in Python',
-        subtitle: 'Python',
-        image: './images/dj-ads.jpg',
-        link: "#",
-        date: "2022-04-30T19:50:28.346094Z",
-        description:'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...',
-        featured: false,     
-    }
 ]
