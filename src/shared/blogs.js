849113d12c0ff25b8df47401828691da86d10f8e
Ivan Bowen <bowenivan16@gmail.com>
2023-01-21 15:31:37 +0300

    update blogs data

--- src/shared/blogs.js
+++ src/shared/blogs.js
@@ -1,6 +1,16 @@
 export const BLOGS = [
     {
         id: 1,
+        title:'How to build an e-commerce Next.js application with xata and cloudinary',
+        subtitle: 'Next.js',
+        image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--0oYv82i5--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/kgbyyrqu0dgt5f2xf1ke.png',
+        link: "https://medium.com/@ivan_bowen/docstrings-in-python-for-beginners-9bdd2657f8c4",
+        date: "2023-01-01T19:50:28.346094Z",
+        description:'When building an e-commerce application backend development is important. Developers need to come up with complex functions and this might be time-consuming and costly. Serverless ...',
+        featured: true,     
+    },
+    {
+        id: 2,
         title:'Object Oriented Programming in Python',
         subtitle: 'Python',
         image: 'https://res.cloudinary.com/bowenivan/image/upload/q_auto:best/v1674296977/Articles/bowenivan.dev/programming_approaches_ujqnzl.jpg',
@@ -10,7 +20,7 @@ export const BLOGS = [
         featured: false,     
     },
     {
-        id: 2,
+        id: 3,
         title:'Docstrings in Python for begginners',
         subtitle: 'Python',
         image: 'https://res.cloudinary.com/bowenivan/image/upload/q_auto:best/v1674296868/Articles/bowenivan.dev/docpy_r8oc1d.jpg',
@@ -18,15 +28,5 @@ export const BLOGS = [
         date: "2022-04-23T19:50:28.346094Z",
         description:'So, what exactly is documentation? What are docstrings? Why is it recommended more frequently during Python programming documentation? What is the best method for documenting your ...',
         featured: false,     
-    },
-    {
-        id: 3,
-        title:'How to build an e-commerce Next.js application with xata and cloudinary',
-        subtitle: 'Next.js',
-        image: 'https://res.cloudinary.com/practicaldev/image/fetch/s--0oYv82i5--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/kgbyyrqu0dgt5f2xf1ke.png',
-        link: "https://medium.com/@ivan_bowen/docstrings-in-python-for-beginners-9bdd2657f8c4",
-        date: "2023-01-01T19:50:28.346094Z",
-        description:'When building an e-commerce application backend development is important. Developers need to come up with complex functions and this might be time-consuming and costly. Serverless ...',
-        featured: true,     
     }
 ]
